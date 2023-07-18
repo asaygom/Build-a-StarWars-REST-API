8c65478297f348423fdb013466b8be1db66e0427
Alexis Sayago <102667039+asaygom@users.noreply.github.com>
2023-07-18 01:35:07 +0000

    api working

--- src/app.py
+++ src/app.py
@@ -2,27 +2,23 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
-from flask import Flask, request, jsonify, url_for
-from flask_migrate import Migrate
+from admin import setup_admin
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
-from admin import setup_admin
-from models import db, User
+from flask import Flask, request, jsonify, url_for
+from models import db, User, Character, Planet, Favorite
+from flask_migrate import Migrate
 #from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
-db_url = os.getenv("DATABASE_URL")
-if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
-else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-MIGRATE = Migrate(app, db)
 db.init_app(app)
+migrate = Migrate(app, db)
 CORS(app)
 setup_admin(app)
 
@@ -36,16 +32,184 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/user', methods=['GET', "POST"])
+def handle_user():
+    if request.method == 'GET':
+        users = User.query.all()
+        users = list(map(lambda user: user.to_dict(), users))
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+        return jsonify({
+            "data": users
+        }), 200
+    elif request.method == 'POST':
+        print("Prueba2")
+        user = User()
+        #data = request.json.get()
+        user.name = request.json.get("name")
+        user.username = request.json.get("username")
+        user.password = request.json.get("password")
 
-    return jsonify(response_body), 200
+        db.session.add(user)
+        db.session.commit()
 
+        return jsonify({
+            "msg": "user created"
+        }), 200
+
+@app.route("/user/<int:id>", methods=["PUT", "DELETE"])
+def update_user(id):
+    if request.method == 'PUT':
+        user = User.query.get(id)
+        if user is not None:
+            data = request.get_json()
+            user.name = data["name"]
+            user.username = data["username"]
+            db.session.commit()
+            return jsonify({
+                "msg":"user updated"
+            }),200
+        else:
+            return jsonify({
+                "msg": "user not found"
+            }), 404
+    elif request.method == 'DELETE':
+        user = User.query.get(id)
+        if user is not None:
+            db.session.delete(user)
+            db.session.commit()
+
+            return jsonify({
+                "msg":"user deleted"
+            }),202
+        else:
+            return jsonify({
+                "msg":"user not found"
+            }), 404
+
+@app.route('/character', methods=['GET', "POST"])
+def handle_character():
+    if request.method == 'GET':
+        characters = Character.query.all()
+        characters = list(map(lambda character: character.to_dict(), characters))
+
+        return jsonify({
+            "data": characters
+        }), 200
+    elif request.method == 'POST':
+        character = Character()
+        data = request.get_json()
+        character.name = data["name"]
+
+        db.session.add(character)
+        db.session.commit()
+
+        return jsonify({
+            "msg": "character created"
+        }), 200
+
+@app.route("/character/<int:id>", methods=["PUT", "DELETE"])
+def update_character(id):
+    if request.method == 'PUT':
+        character = Character.query.get(id)
+        if character is not None:
+            data = request.get_json()
+            character.name = data["name"]
+            db.session.commit()
+            return jsonify({
+                "msg":"character updated"
+            }),200
+        else:
+            return jsonify({
+                "msg": "character not found"
+            }), 404
+    elif request.method == 'DELETE':
+        character = Character.query.get(id)
+        if character is not None:
+            db.session.delete(character)
+            db.session.commit()
+
+            return jsonify({
+                "msg":"character deleted"
+            }),202
+        else:
+            return jsonify({
+                "msg":"character not found"
+            }), 404
+
+@app.route('/planet', methods=['GET', "POST"])
+def handle_planet():
+    if request.method == 'GET':
+        planets = Planet.query.all()
+        planets = list(map(lambda planet: planet.to_dict(), planets))
+
+        return jsonify({
+            "data": planets
+        }), 200
+    elif request.method == 'POST':
+        planet = Planet()
+        data = request.get_json()
+        planet.name = data["name"]
+
+        db.session.add(planet)
+        db.session.commit()
+
+        return jsonify({
+            "msg": "planet created"
+        }), 200
+
+@app.route("/planet/<int:id>", methods=["PUT", "DELETE"])
+def update_planet(id):
+    if request.method == 'PUT':
+        planet = Planet.query.get(id)
+        if planet is not None:
+            data = request.get_json()
+            planet.name = data["name"]
+            db.session.commit()
+            return jsonify({
+                "msg":"planet updated"
+            }),200
+        else:
+            return jsonify({
+                "msg": "planet not found"
+            }), 404
+    elif request.method == 'DELETE':
+        planet = Planet.query.get(id)
+        if planet is not None:
+            db.session.delete(planet)
+            db.session.commit()
+
+            return jsonify({
+                "msg":"planet deleted"
+            }),202
+        else:
+            return jsonify({
+                "msg":"planet not found"
+            }), 404
+
+@app.route('/user/<int:id>/favorite', methods=['GET'])
+def handle_favorite():
+    favorites = Favorite.query.all()
+    favorites = list(map(lambda favorite: favorite.to_dict(), favorites))
+
+    return jsonify({
+        "data": favorites
+    }), 200
+    
+@app.route('/user/<int:id>/favorite', methods=["POST"])
+def handle_favorite():
+    favorite = Favorite()
+    data = request.get_json()
+    if 
+    favorite.user_id = data["name"]
+
+    db.session.add(favorite)
+    db.session.commit()
+
+    return jsonify({
+        "msg": "favorite created"
+    }), 200
+
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=5000)
 # this only runs if `$ python src/app.py` is executed
-if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=PORT, debug=False)
--- src/models.py
+++ src/models.py
@@ -3,17 +3,53 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String(250), nullable=False)
+    username = db.Column(db.String(250), nullable=False, unique=True)
+    password = db.Column(db.String(250), nullable=False)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
-
-    def serialize(self):
-        return {
+    def to_dict(self):
+        return{
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "name": self.name,
+            "username": self.username
+        }
+
+class Character(db.Model):
+    __tablename__ = 'character'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+
+    def to_dict(self):
+        return{
+            "id": self.id,
+            "name": self.name
+        }
+
+class Planet(db.Model):
+    __tablename__ = 'planet'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+
+    def to_dict(self):
+        return{
+            "id": self.id,
+            "name": self.name
+        }
+
+class Favorite(db.Model):
+    __tablename__ = 'favorite'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_character = db.Column(db.String, db.ForeignKey('character.id'))
+    user_planet = db.Column(db.String, db.ForeignKey('planet.id'))
+    user = db.relationship(User)
+
+    def to_dict(self):
+        return{
+            "id": self.id,
+            "user_id": self.user_id,
+            "user_character": self.user_character,
+            "user_planet": self.user_planet
         }
